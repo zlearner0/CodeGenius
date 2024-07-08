f01ab8c84abf093900430cbd24e2547808e189b9
ahmed <ahelmyeng1@gmail.com>
2024-07-08 05:39:00 -0700

    update init.py with sqlalchemy setting

--- project/__init__.py
+++ project/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
-
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = '3782194d5c1a4f2g69531e2l890k751f'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
+db = SQLAlchemy(app)
 
 from project import routes
