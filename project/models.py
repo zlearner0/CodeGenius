406771a7831fb19478c4f68b0830b4fe71d3e6c1
ahmed <ahelmyeng1@gmail.com>
2024-07-09 10:28:55 -0700

    adding User class for the database file models.py

--- project/models.py
+++ project/models.py
@@ -1,23 +1,7 @@
-from project import db, login_manager
-from flask_login import UserMixin
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-class User_Course(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    course_id = db.Column(db.Integer, db.ForeignKey('course.id'))
-
-    the_user = db.relationship("User", backref="x")
-    the_course = db.relationship("Course", backref="x")
+from project import db
 
 
-
-class User(db.Model, UserMixin):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
