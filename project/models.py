84025771ea5e406b97007272de50055a6230a06c
ahmed <ahelmyeng1@gmail.com>
2024-07-10 10:39:56 -0700

    adding Course class for database

--- project/models.py
+++ project/models.py
@@ -6,3 +6,9 @@ class User(db.Model):
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
+
+
+
+class Course(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(20), unique=True, nullable=False)
