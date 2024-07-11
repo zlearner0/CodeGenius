99b304933f438c17e062afc98fd01c6f950fc2c2
ahmed <ahelmyeng1@gmail.com>
2024-07-11 07:41:25 -0700

    update database tables with association table and init.py

--- project/__init__.py
+++ project/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '3782194d5c1a4f2g69531e2l890k751f'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
 
 from project import routes
--- project/models.py
+++ project/models.py
@@ -1,7 +1,23 @@
-from project import db
+from project import db, login_manager
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User_Course(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    course_id = db.Column(db.Integer, db.ForeignKey('course.id'))
+
+    the_user = db.relationship("User", backref="x")
+    the_course = db.relationship("Course", backref="x")
 
 
-class User(db.Model):
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
